8641ae841915f9ad1e9ff8de5bb35c7d93d32129
Max Mawerid <monasmax02@gmail.com>
2023-01-06 18:53:11 +0300

    Connect my repo

--- src/main/scala/clustering.scala
+++ src/main/scala/clustering.scala
@@ -1,5 +1,9 @@
-object clustering extends App{
 
-  println("Mawerid, hello!")
+object clustering {
+
+  // делаю функции для ввода и вывода данных + функция main
+  def main(args: Array[String]): Unit = {
+    print("hello from the main method")
+  }
 
 }
