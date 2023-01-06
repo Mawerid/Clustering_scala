10414530cbb0a92965e48548df493551c8f7961a
Daniil178 <daniiluskov337@gmail.com>
2023-01-06 15:13:38 +0300

    some updates

---
+++ src/main/scala/functions/distance.scala
@@ -0,0 +1,12 @@
+package functions
+
+object distance extends App{
+
+  val p1: Array[Double] = Array(1, 1, 1, 1)
+  val p2: Array[Double] = Array(2, 2, 2, 2)
+
+  //def diff: (Array[Double], Array[Double])
+
+  //val p: Array[Double] = p1.flatMap(x1 => )
+
+}
---
+++ src/test/scala/testingCode.scala
@@ -0,0 +1,3 @@
+object testingCode extends App{
+  println("Hello, World!")
+}
