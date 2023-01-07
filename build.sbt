452ee6b85d674afe0a89f30dfd1899f848accdf5
Max Mawerid <monasmax02@gmail.com>
2023-01-08 00:00:05 +0300

    MAKE BRAND NEW VIS

--- build.sbt
+++ build.sbt
@@ -12,8 +12,4 @@ ThisBuild / resolvers += Resolver.bintrayRepo("cibotech", "public")
 ThisBuild / libraryDependencies ++= Seq(
   "com.github.tototoshi" %% "scala-csv" % "1.3.10",
   "com.github.haifengl" %% "smile-scala" % "3.0.0"
-//  "io.github.cibotech" %% "evilplot" % "0.8.1",
-//  "com.cibo" %% "evilplot-repl" % "0.6.3"
-  //  "io.github.pityka" %% "nspl-awt" % "0.9.0",
-//  "io.github.pityka" %% "nspl-canvas-js" % "0.9.0"
 )
--- src/main/scala/clustering.scala
+++ src/main/scala/clustering.scala
@@ -2,23 +2,10 @@ import io.Input._
 import algorithm.KMeans._
 import io.Visualisation._
 import io.Output._
+
 import java.util.concurrent.{ExecutorService, Executors}
 import smile.data.DataFrame
-import smile.read
-//import com.cibo.evilplot.plot.renderers.PointRenderer
 
-//import scala.annotation.tailrec
-//import scala.reflect.runtime.universe.Try
-//import io.Visualisation._
-//import io.Output._
-//import scala.util.Random
-
-//import io.displayPlot
-//import com.cibo.evilplot.plot._
-//import com.cibo.evilplot.plot.aesthetics.DefaultTheme._
-//import com.cibo.evilplot.numeric.Point
-
-import scala.math._
 import java.awt.Color._
 import smile.plot._
 import smile.plot.swing._
@@ -28,60 +15,43 @@ object clustering {
 
   def main(args: Array[String]): Unit = {
 
-    //Кол-во доступных ядер для распараллеливания
+    // Кол-во доступных ядер для распараллеливания
     val countThread = 4 //java.lang.Runtime.getRuntime.availableProcessors - 1
 
-    //Чтение данных
-    val path = "data/input/clusters_2_dim_2.csv"
+    // Чтение данных
+    val path = "data/input/clusters_3_dim_3.csv"
     val data = readFullCSV(path)
+    val clusterNum = 3
+
+    // Запуск расчетов
     val pool = Executors.newFixedThreadPool(countThread)
 
-    val clusters = KMeans(data, 2, 0.00001, pool, countThread)
-    val clusterNum = 2
+    val clusters = KMeans(data, clusterNum, 0.00001, pool, countThread)
 
     println(clusters.size)
     clusters.foreach(it => println(it.size))
 
     pool.shutdown()
 
-    //Визуализация данных
-//    val scatter = drawScatterToByteArray(data
-//      , "X"
-//      , "Y"
-//      , "Clustering data (2 clusters, 2 dimensions)"
-//      , "Some data")
-//    val filename = "data/output/pic.png"
-//    saveScatterPlot(scatter, filename)
-    val names: Seq[String] = (0 until clusterNum).map(i => s"Coordinate $i")
+    // Визуализация данных
+    val names: Seq[String] = data.head.indices.map(i => s"coordinate $i") :+ "class"
     val transData = data.map(_.toArray).toArray
-    val df = DataFrame.of(transData, names: _*)
-//    val tmp = DataFrame.
 
+    val tmp = data
+      .map(point => (0 until clusterNum)
+        .map(i => if (clusters(i)
+          .contains(point)) i.toDouble + 1d))
+      .map(it => it
+        .find(_.toString != "()")
+        .get)
+      .map(_.toString.toDouble).toArray
+
+    val df = DataFrame.of(transData.zip(tmp).map(it => it._1 :+ it._2), names: _*)
     println(df)
 
-    val canvas = plot(df, "Coordinate 0", "Coordinate 1", "", '*')
-    canvas.setAxisLabels("X", "Y")
-    show(canvas)
-
-    //    val heart = -314 to 314 map { i =>
-    //      val t = i / 100.0
-    //      val x = 16 * pow(sin(t), 3)
-    //      val y = 13 * cos(t) - 5 * cos(2 * t) - 2 * cos(3 * t) - cos(4 * t)
-    //      Array(x, y)
-    //    }
-    //    show(line(heart.toArray, color = RED))
-
-
-    //    //Визуализация данных
-    //    val scatter = drawScatterToByteArray(data
-    //      , "X"
-    //      , "Y"
-    //      , "Clustering data (2 clusters, 2 dimensions)"
-    //      , "Some data")
-    //    val filename = "data/output/pic.png"
-    //    saveScatterPlot(scatter, filename)
-    //
-    //    val scatterCluster =
+    val plotBefore = plot(df, "coordinate 0", "coordinate 1", "coordinate 2","class", '*')
+    plotBefore.setAxisLabels("X", "Y", "Z")
+    show(plotBefore)
   }
 
 }
