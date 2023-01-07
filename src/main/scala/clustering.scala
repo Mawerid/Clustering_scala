0a904b4562f48dacec188737b5d8e07a44804cbd
Max Mawerid <monasmax02@gmail.com>
2023-01-07 10:42:16 +0300

    Rework some parts

--- src/main/scala/clustering.scala
+++ src/main/scala/clustering.scala
@@ -1,16 +1,30 @@
 import io.Input._
-import io.Visualisation
+import io.Visualisation._
 import io.Output._
 
 object clustering {
 
-  // делаю функции для ввода и вывода данных + функция main теперь у нас есть)
   def main(args: Array[String]): Unit = {
 
-    val path = "data/input/clusters_2_dim_2.csv"
+    //Кол-во доступных ядер для распараллеливания
+    val countProcessor = java.lang.Runtime.getRuntime.availableProcessors - 1
 
+    println(countProcessor)
+
+    //Чтение данных
+    val path = "data/input/clusters_2_dim_2.csv"
+    val data = readFullCSV(path)
+
+    //Визуализация данных
+    val scatter = drawScatterToByteArray(data
+      , "X"
+      , "Y"
+      , "Clustering data (2 clusters, 2 dimentions)"
+      ,"Some data")
     val filename = "data/output/pic.png"
-//    saveScatterPlot(scatter, filename)
+    saveScatterPlot(scatter, filename)
+
+
   }
 
 }
--- src/main/scala/io/Visualisation.scala
+++ src/main/scala/io/Visualisation.scala
@@ -5,21 +5,26 @@ import org.nspl.awtrenderer._
 
 object Visualisation {
 
-  // Git'у не нравится почему-то, хотя все работает
-//  def drawScatterToByteArray(data: List[List[Double]], xLabel: String, yLabel: String, title: String): Array[Byte] = {
-//
-//    val df = data.map(lst => lst.head -> lst(1))
-//
-//    val plot = xyplot(df)(
-//      par(
-//        main = title,
-//        xlab = xLabel,
-//        ylab = yLabel
-//      )
-//    )
-//
-//    pngToByteArray(
-//      plot.build,
-//      width = 2000)
-//  }
+  // Вроде как теперь все работает
+  def drawScatterToByteArray(data: List[List[Double]], xLabel: String, yLabel: String, title: String, legend: String): Array[Byte] = {
+
+    val df = data.map(lst => lst.head -> lst(1))
+
+    val plot = xyplot((df, List(point()), InLegend(legend)))(
+      par(
+        main = title,
+        xlab = xLabel,
+        ylab = yLabel
+      )
+    )
+
+    val builtPlot = plot.build
+//    implicit val renderer: Renderer[Elems2[XYPlotArea, Legend], JavaRC] = {
+//      Renderer(builtPlot, new JavaRC())
+//    }
+
+    pngToByteArray(
+      builtPlot,
+      width = 2000)//(renderer)
+  }
 }
