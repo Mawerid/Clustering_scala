89deb6bb27916d14924ef09b032a4470db5d6027
Max Mawerid <monasmax02@gmail.com>
2023-01-07 22:55:30 +0300

    Try to solve troubles with visualisation

--- src/main/scala/clustering.scala
+++ src/main/scala/clustering.scala
@@ -1,7 +1,25 @@
 import io.Input._
 import algorithm.KMeans._
-import io.Visualisation._
-import io.Output._
+import smile.data.DataFrame
+import smile.read
+//import com.cibo.evilplot.plot.renderers.PointRenderer
+
+//import scala.annotation.tailrec
+//import scala.reflect.runtime.universe.Try
+//import io.Visualisation._
+//import io.Output._
+//import scala.util.Random
+
+//import io.displayPlot
+//import com.cibo.evilplot.plot._
+//import com.cibo.evilplot.plot.aesthetics.DefaultTheme._
+//import com.cibo.evilplot.numeric.Point
+
+import scala.math._
+import java.awt.Color._
+import smile.plot._
+import smile.plot.swing._
+import smile.plot.Render._
 
 object clustering {
 
@@ -16,19 +34,42 @@ object clustering {
     val path = "data/input/clusters_2_dim_2.csv"
     val data = readFullCSV(path)
 
-    val clusters = KMeans(data, 2)
+    val clusterNum = 2
+    val clusters = KMeans(data, clusterNum)
 
     println(clusters.size)
     clusters.foreach(it => println(it.size))
 
-    //Визуализация данных
-//    val scatter = drawScatterToByteArray(data
-//      , "X"
-//      , "Y"
-//      , "Clustering data (2 clusters, 2 dimensions)"
-//      , "Some data")
-//    val filename = "data/output/pic.png"
-//    saveScatterPlot(scatter, filename)
+    val names: Seq[String] = (0 until clusterNum).map(i => s"Coordinate $i")
+    val transData = data.map(_.toArray).toArray
+    val df = DataFrame.of(transData, names: _*)
+//    val tmp = DataFrame.
+
+    println(df)
+
+    val canvas = plot(df, "Coordinate 0", "Coordinate 1", "", '*')
+    canvas.setAxisLabels("X", "Y")
+    show(canvas)
+
+    //    val heart = -314 to 314 map { i =>
+    //      val t = i / 100.0
+    //      val x = 16 * pow(sin(t), 3)
+    //      val y = 13 * cos(t) - 5 * cos(2 * t) - 2 * cos(3 * t) - cos(4 * t)
+    //      Array(x, y)
+    //    }
+    //    show(line(heart.toArray, color = RED))
+
+
+    //    //Визуализация данных
+    //    val scatter = drawScatterToByteArray(data
+    //      , "X"
+    //      , "Y"
+    //      , "Clustering data (2 clusters, 2 dimensions)"
+    //      , "Some data")
+    //    val filename = "data/output/pic.png"
+    //    saveScatterPlot(scatter, filename)
+    //
+    //    val scatterCluster =
   }
 
 }
--- src/main/scala/io/Visualisation.scala
+++ src/main/scala/io/Visualisation.scala
@@ -1,36 +1,65 @@
 package io
 
-import org.nspl._
-import org.nspl.awtrenderer._
+//import io.displayPlot
+//import com.cibo.evilplot.plot._
+//import com.cibo.evilplot.plot.aesthetics.DefaultTheme._
+//import com.cibo.evilplot.numeric.Point
 
-object Visualisation {
+object Visualisation extends App {
+//
+//  val data = Seq.tabulate(100) { i =>
+//    Point(i.toDouble, scala.util.Random.nextDouble())
+//  }
+//  displayPlot(ScatterPlot(data)
+//    .xAxis()
+//    .yAxis()
+//    .frame()
+//    .xLabel("x")
+//    .yLabel("y")
+//    .render())
 
-  /**
-   * generate plot from 2 dimension data
-   *
-   * @param data   data
-   * @param xLabel label of x axes
-   * @param yLabel label of y axes
-   * @param title  main title of plot
-   * @param legend legend of plot
-   * @return png plot as ByteArray
-   */
-  def drawScatterToByteArray(data: List[List[Double]], xLabel: String, yLabel: String, title: String, legend: String): Array[Byte] = {
+//  /**
+//   * generate plot from 2 dimension data
+//   *
+//   * @param data   data
+//   * @param xLabel label of x axes
+//   * @param yLabel label of y axes
+//   * @param title  main title of plot
+//   * @param legend legend of plot
+//   * @return png plot as ByteArray
+//   */
+//  def drawScatterToByteArray(data: List[List[List[Double]]], xLabel: String, yLabel: String, title: String, legend: String): Array[Byte] = {
+//
+//    val df = data.map(lst => lst.head -> lst(1))
+//
+//    val plot = xyplot((df, List(point()), InLegend(legend)))(
+//      par(
+//        main = title,
+//        xlab = xLabel,
+//        ylab = yLabel
+//      )
+//    )
+//
+//    val fig0 = xyplot(
+//      df("Sepal.Length", "Sepal.Width", "spec")
+//    )(
+//      par(extraLegend = spec2Num.toSeq.map(x =>
+//        x._1 -> PointLegend(
+//          shape = Shape.rectangle(0, 0, 1, 1),
+//          color = DiscreteColors(spec2Num.size)(x._2)
+//        )
+//      ),
+//        xlab = "Sepal.Length",
+//        ylab = "Sepal.Width",
+//        main = "Iris data")
+//    )
+//
+//    val builtPlot = plot.build
+//
+//    pngToByteArray(
+//      builtPlot,
+//      width = 2000)
+//  }
 
-    val df = data.map(lst => lst.head -> lst(1))
 
-    val plot = xyplot((df, List(point()), InLegend(legend)))(
-      par(
-        main = title,
-        xlab = xLabel,
-        ylab = yLabel
-      )
-    )
-
-    val builtPlot = plot.build
-
-    pngToByteArray(
-      builtPlot,
-      width = 2000)
-  }
 }
