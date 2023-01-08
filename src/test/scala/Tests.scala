2c7073b3f585257ce5549d5ca3dbd9fee54cf236
Daniil178 <daniiluskov337@gmail.com>
2023-01-08 18:41:05 +0300

    add tests

---
+++ src/test/scala/Tests.scala
@@ -0,0 +1,13 @@
+import io.Input._
+import io.Visualisation._
+import io.Output._
+
+import algorithm.KMeans._
+import algorithm.SequentialJoins._
+import algorithm.AffinityPropagation._
+
+import java.util.concurrent.Executors
+
+object Tests {
+
+}
